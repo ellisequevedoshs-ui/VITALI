e2f3adf98eca13224479108679b97348387e537a
Keith Bartido <bartidokpi@gmail.com>
2026-04-05 21:38:54 +0800

    fix firebase db for user signups and login, dashboard functionality, and automatic redirect to consent.html

--- vitali-deploy/firebase.js
+++ vitali-deploy/firebase.js
@@ -1,32 +1,38 @@
+// const firebaseConfig = {
+
+// apiKey: "AIzaSyCbZk0wgofX4PMusy7DFsy8RKpxnx5FcGY",
+
+// authDomain: "vitali-17a9e.firebaseapp.com",
+
+// projectId: "vitali-17a9e",
+
+// storageBucket: "vitali-17a9e.firebasestorage.app",
+
+// messagingSenderId: "41512254831",
+
+// appId: "1:41512254831:web:9edb65a58e09646bd4bc9a",
+
+// };
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-
 const firebaseConfig = {
-
-apiKey: "AIzaSyCbZk0wgofX4PMusy7DFsy8RKpxnx5FcGY",
-
-authDomain: "vitali-17a9e.firebaseapp.com",
-
-projectId: "vitali-17a9e",
-
-storageBucket: "vitali-17a9e.firebasestorage.app",
-
-messagingSenderId: "41512254831",
-
-appId: "1:41512254831:web:9edb65a58e09646bd4bc9a",
-
+  apiKey: "AIzaSyBIEk5MXcmWXM7gaFu0JSo8gBAlj6s0ELk",
+  authDomain: "test-4dc61.firebaseapp.com",
+  projectId: "test-4dc61",
+  storageBucket: "test-4dc61.firebasestorage.app",
+  messagingSenderId: "249547404250",
+  appId: "1:249547404250:web:9317bc3bd0a6a1b9c906e3",
 };
-
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
 const db = getFirestore(app);
-
 
 export { auth, db };
